e43eef94ce4b4903820a777e81a8b9cde2464daf
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-31 17:16:31 +0400

    Added design for try, win and lose notifications

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ function guessNumberGame() {
     for (maxNumber; maxNumber != -1; maxNumber--) {
         for (tries = tries; tries != -1; tries--) {
             let yourNumber = parseInt(prompt(`Write your number, ${tries} left`))
+            // let yourNumber = document.getElementById('inputRow').value;
             if (yourNumber < hiddenNumber) {
-                alert(`Try a bigger number, ${tries} left`)
+                document.getElementById('result').innerHTML = `Try a bigger number, ${tries} left`
             } else if (yourNumber > hiddenNumber) {
-                alert(`Try a smaller number, ${tries} left`)
+                document.getElementById('result').innerHTML = `Try a smaller number, ${tries} left`
             }
             if (yourNumber === hiddenNumber) {
-                alert(`CONGRATULATIONS YOU WIN! \n The number was ${hiddenNumber}`)
+                document.getElementById('result').innerHTML = `CONGRATULATIONS YOU WIN! \n The number was ${hiddenNumber}`
                 break
             } else if (tries === 0) {
-                alert(`You lose the number was ${hiddenNumber}`)
+                document.getElementById('result').innerHTML = `You lose the number was ${hiddenNumber}`
                 break
             }
         }
