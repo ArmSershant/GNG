8dad9065a6553d9c424adb67f0b34fc33c3167aa
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-31 17:47:30 +0400

    added input for player number (working partly)

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function guessNumberGame() {
     // let maxNumber = parseInt(prompt(`Write the max number`))
+    function check(){
     let maxNumber = document.getElementById('maxNumber').value;
     // let tries = parseInt(prompt(`Write the number of tries`))
     let tries = document.getElementById('tries').value;
     let hiddenNumber = Math.floor(Math.random() * maxNumber + 1)
     for (maxNumber; maxNumber != -1; maxNumber--) {
         for (tries = tries; tries != -1; tries--) {
-            let yourNumber = parseInt(prompt(`Write your number, ${tries} left`))
-            // let yourNumber = document.getElementById('inputRow').value;
+            // let yourNumber = parseInt(prompt(`Write your number, ${tries} left`))
+            let yourNumber = document.getElementById('inputRow').value;
             if (yourNumber < hiddenNumber) {
                 document.getElementById('result').innerHTML = `Try a bigger number, ${tries} left`
             } else if (yourNumber > hiddenNumber) {
                 document.getElementById('result').innerHTML = `Try a smaller number, ${tries} left`
             }
             if (yourNumber === hiddenNumber) {
-                document.getElementById('result').innerHTML = `CONGRATULATIONS YOU WIN! \n The number was ${hiddenNumber}`
+                document.getElementById('result').innerHTML = `CONGRATULATIONS YOU WIN! \n    The number was ${hiddenNumber}`
                 break
             } else if (tries === 0) {
                 document.getElementById('result').innerHTML = `You lose the number was ${hiddenNumber}`
@@ -22,5 +23,6 @@ function guessNumberGame() {
             }
         }
         break
+    }
     }
 }
