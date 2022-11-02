b52706022adbda55b35dcd7a674525291ece6024
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-11-02 19:57:23 +0400

    solved some bugs

--- src/Game.js
+++ src/Game.js
@@ -10,18 +10,14 @@ export class Game {
         for (maxNumber; maxNumber != -1; maxNumber--) {
             for (tries = tries; tries != -1; tries--) {
                 let yourNumber = +prompt(`Write your number, ${tries} left`)
-                switch (yourNumber) {
-                    case yourNumber < hiddenNumber:
-                        alert(`Try a bigger number, ${tries} left`)
-                        break
-                    case yourNumber > hiddenNumber:
-                        alert(`Try a smaller number, ${tries} left`)
-                        break
-                    case yourNumber == hiddenNumber:
-                        alert(`CONGRATULATIONS YOU WIN! The hidden number was ${hiddenNumber}`)
-                        break
+                if (yourNumber < hiddenNumber) {
+                    alert(`Try a bigger number`)
+                } else if (yourNumber > hiddenNumber) {
+                    alert(`Try a smaller number`)
                 }
-                if (tries == 0) {
+                if (yourNumber == hiddenNumber) {
+                    alert(`CONGRATULATIONS YOU WIN! The hidden number was ${hiddenNumber}`)
+                } else if (tries == 0) {
                     alert(`You lose the number was ${hiddenNumber}`)
                 }
             }
